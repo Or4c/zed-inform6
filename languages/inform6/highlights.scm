12fc60b6c8c389c264c50535ebf26cc086e207a2
Sam Win-Mason <sam@albedosoftware.com>
2024-08-10 19:27:14 +1200

    WIP on hilighting

--- languages/inform6/highlights.scm
+++ languages/inform6/highlights.scm
@@ -1,1 +1,76 @@
 (number) @number
+(identifier) @variable
+(constant) @constant
+(comment) @comment
+(string_double_quoted) @string
+(string_single_quoted) @string
+(operator) @operator
+(boolean) @boolean
+(global) @constructor
+(include) @constructor
+(replace) @constructor
+(serial) @constant
+(release (number) @number) @constant
+(nothing) @keyword
+(action) @function
+[
+    "++"
+    "--"
+] @operator
+
+; Types
+[
+    (grammar)
+    (attribute)
+    (array)
+] @type
+
+(object class_name: (identifier) @type)
+(routine_statement (identifier)) @function
+(routine_message) @function
+(array_access) @property
+(property_access (_)(identifier)) @property
+(function_sig) @function
+(embedded_routine) @function
+
+; Punctuation
+[
+  ";"
+  "."
+  ","
+] @punctuation.delimiter
+
+[
+  "{"
+  "}"
+] @punctuation.bracket
+
+; Keywords
+[
+    "with"
+    "has"
+    "private"
+    "class"
+    "if"
+    "objectloop"
+    "else"
+    "switch"
+    "do"
+    "until"
+    "while"
+    "move"
+    "remove"
+    "to"
+    "break"
+    "give"
+    "return"
+    "rtrue"
+    "rfalse"
+    "print"
+    "print_ret"
+    "box"
+    "new_line"
+    "spaces"
+    "noun"
+    "scope"
+] @keyword
