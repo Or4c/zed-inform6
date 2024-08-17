ca686ec9f2f61d465c881acf9a46474c96ba1cc9
Sam Win-Mason <sam@albedosoftware.com>
2024-08-17 12:54:46 +1200

    added indents for routines, objects, grammar clauses and code blocks in conditionals.

---
+++ languages/inform6/indents.scm
@@ -0,0 +1,11 @@
+; Routine
+
+(function_block) @indent
+(routine "]" @end) @indent
+
+; Objects
+(object ";" @end) @indent
+(embedded_routine "]" @end) @indent
+
+(grammar_clause) @indent
+(code_block "}" @end) @indent
